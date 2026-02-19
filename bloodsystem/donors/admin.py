84857d25991d04e6b6bede335064c0c9f98d3053
Abdulrahman Dawoud <abdulrahman.goldendawn@gmail.com>
2026-02-20 02:36:22 +0600

    registered my models in admin.py

--- bloodsystem/donors/admin.py
+++ bloodsystem/donors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Donor, BloodRequest, HospitalDonorRequest
 
-# Register your models here.
+admin.site.register(Donor)
+admin.site.register(BloodRequest)
+admin.site.register(HospitalDonorRequest)
+
