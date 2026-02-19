889e5d2643b86b920a81fca47eff54bd609278eb
Abdulrahman Dawoud <abdulrahman.goldendawn@gmail.com>
2026-02-20 01:19:50 +0600

    added the database

--- bloodsystem/bloodsystem/settings.py
+++ bloodsystem/bloodsystem/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "bloodsystem.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blood_db',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
